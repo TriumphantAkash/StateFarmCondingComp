bfc8c4589cc6092fcc4690518e8e271cdbb66a80
Akash Chaturvedi <axc144430@utdallas.edu>
2015-10-17 23:20:24 -0500

    mostActiveSender() done

--- src/main/java/com/statefarm/codingcomp/QueryServiceImpl.java
+++ src/main/java/com/statefarm/codingcomp/QueryServiceImpl.java
@@ -1,9 +1,13 @@
 package com.statefarm.codingcomp;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import com.statefarm.codingcomp.model.Email;
@@ -37,6 +41,30 @@ public class QueryServiceImpl implements QueryService {
     			userList.add(user);
     		}
     	}    	
+    	for(int i = 0; i< str2.length; i++) {
+    		if(str2[i].contains(domain)) {
+    			String []userInfo = str2[i].split(",");
+    			User user = new User(userInfo[1], userInfo[2]);
+    			
+    			userList.add(user);
+    		}
+    	}  
+    	for(int i = 0; i< str3.length; i++) {
+    		if(str3[i].contains(domain)) {
+    			String []userInfo = str3[i].split(",");
+    			User user = new User(userInfo[1], userInfo[2]);
+    			
+    			userList.add(user);
+    		}
+    	}  
+    	for(int i = 0; i< str4.length; i++) {
+    		if(str4[i].contains(domain)) {
+    			String []userInfo = str4[i].split(",");
+    			User user = new User(userInfo[1], userInfo[2]);
+    			
+    			userList.add(user);
+    		}
+    	}  
     	return userList; 
     }
 
@@ -96,7 +124,50 @@ public class QueryServiceImpl implements QueryService {
 
     @Override
     public String mostActiveSender() throws Exception {
+        // TODO Auto-generated method stub    	
+    	Map<String, Integer> countUser = new HashMap<String, Integer>();
+    	List<String> allSenders = new ArrayList<>();
+    	int count = 0;
+    	
+    	String []str1;
+    	String []str2; 
+    	String []str3;
+    	String []str4;
+    	
+    	
         // TODO Auto-generated method stub
+    	str1 = reader.read(1, "mail.txt");	//file 1
+    	str2 = reader.read(2, "mail.txt");  //file 2
+    	str3 = reader.read(3, "mail.txt");  //file 3
+    	str4 = reader.read(4, "mail.txt");   	//file 4
+        
+    	for(int i = 0; i< str1.length; i++) {
+    			String []mailInfo = str1[i].split(",");
+    			allSenders.add(mailInfo[1]);
+    	}
+    	for(int i = 0; i< str2.length; i++) {
+			String []mailInfo = str2[i].split(",");
+			allSenders.add(mailInfo[1]);
+    	}
+    	for(int i = 0; i< str3.length; i++) {
+			String []mailInfo = str3[i].split(",");
+			allSenders.add(mailInfo[1]);
+    	}
+    	for(int i = 0; i< str4.length; i++) {
+			String []mailInfo = str4[i].split(",");
+			allSenders.add(mailInfo[1]);
+    	}
+    	
+    	for (String temp : allSenders) {
+    		countUser.put(temp, Collections.frequency(allSenders, temp));
+    	}
+    	
+    	int maxValueInMap=(Collections.max(countUser.values()));  // This will return max value in the Hashmap
+        for (Entry<String, Integer> entry : countUser.entrySet()) {  // Itrate through hashmap
+            if (entry.getValue()==maxValueInMap) {
+                return entry.getKey();    //return the maximum value
+            }
+        }
         return null;
     }
 
